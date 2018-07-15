9243e20c54e3539cee6c3144ed3f9a6cec69da09
Emmanuel Gonzalez <emmangonza98@gmail.com>
2018-07-15 01:57:13 -0500

    made progress on program1

--- program1/exg5388_p1.s
+++ program1/exg5388_p1.s
@@ -6,7 +6,23 @@
 main:
    BL _scanf
    MOV R1, R0
-   BL _
+   BL _scanf
+   MOV R4, R0
+   BL _scanf
+   MOV R2, R0
+
+   CMP R4, #'+'
+   BEQ _sum
+
+   CMP R4, #'-'
+   BEQ _diff
+
+   CMP R4, #'*'
+   BEQ _mult
+
+
+   CMP R4, #'m'
+   BEQ _min
 
 _exit:
    MOV R7, #1
@@ -23,8 +39,22 @@ _scanf:
    POP {PC}
 
 _sum:
-   
+   MOV R0, R1
+   ADD R0, R2
+   MOV PC, LR
 
+_diff:
+   MOV R0, R1
+   SUB R0, R2
+   MOV PC, LR
+
+_mult:
+   MUL R0, R1, R2
+   MOV PC, LR
+
+_min:
+   CMP R1, R2
+   ...
 
 .data
 format_str:   .asciz   "%d"
