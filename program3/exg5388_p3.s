4e93ddf9ba798df1ec1a06b720cdc04dbc4a3672
Emmanuel Gonzalez <emmanuelgonzalez@Emmanuels-MBP.lan>
2018-07-29 17:47:58 -0500

    added base condition statements

--- program3/exg5388_p3.s
+++ program3/exg5388_p3.s
@@ -8,19 +8,39 @@ main:
    CMP R0, #0
    BEQ _exit
    MOV R4, R0
-   MOV R1, R0
+   MOV R1, R0				@R1 = n
 
    BL _scanf				@checks for second input
    CMP R0, #0
    BEQ _exit
    MOV R4, R0
-   MOV R2, R0
+   MOV R2, R0				@R2 = m
+
+   BL _cnt_part
 
    B main
 
 _exit:
    MOV R7, #1
    SWI 0
+
+_cnt_part:
+   PUSH {LR}
+
+   CMP R1, #0
+
+   MOVEQ R0, #1				@condition 1 (n == 0)
+   POPEQ {PC}
+
+   MOVLT R0, #0				@condition 2 (n < 0)
+   POPLT {PC}
+
+   CMP R2, #0
+   MOVEQ R0, #0
+   POPEQ {PC}
+
+
+
     
 _scanf:
    PUSH {LR}
@@ -41,4 +61,4 @@ _printf:
 
 .data
 format_str:     .asciz      "%d"
-printf_str:     .asciz      "The number entered was: %d\n"
+printf_str:     .asciz      "There are %d partitions of %d using integers up to %d\n"
