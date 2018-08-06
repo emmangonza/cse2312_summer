edbcfc6d878f64817ab9443609989c9849f44387
Emmanuel Gonzalez <emmangonza98@gmail.com>
2018-08-05 23:13:49 -0500

    added a modified version of float_div.s for testing

--- program4/exg5388_p4.s
+++ program4/exg5388_p4.s
@@ -8,16 +8,15 @@ main:
    BL _scanf				@checks for first input, n
    CMP R0, #0
    BEQ _exit
-   MOV R1, R0
 
-   PUSH {R1}				@backup n for _divide call
+   PUSH {R0}				@backup n for _divide call
 
    BL _scanf				@checks for second input, d
    CMP R0, #0
    BEQ _exit
-   MOV R2, R0
+   MOV R1, R0
 
-   POP {R1}
+   POP {R0}
 
    BL _divide
 
@@ -27,14 +26,26 @@ _exit:
    MOV R7, #1
    SWI 0
 
+_scanf:
+    PUSH {LR}                @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                 @ return
+
 _divide:
-   VMOV S0, R1
-   VMOV S1, R2
+   PUSH {LR}
 
-   BL _print_int
+   VMOV S0, R0
+   VMOV S1, R1
 
-   VCVT.F32.S32 S0, S0
-   VCVT.F32.S32 S1, S1
+   @BL _print_int
+
+   VCVT.F32.U32 S0, S0
+   VCVT.F32.U32 S1, S1
 
    VDIV.F32 S2, S0, S1
 
@@ -59,5 +70,7 @@ _print_float:
 
 
 .data
+format_str:     .asciz      "%d"
 result_str1:     .asciz      "%d / %d = "
 result_str2:     .asciz      "%f\n"
+
