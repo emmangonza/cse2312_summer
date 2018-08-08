8286823e8480cfab0922f405ca96aa67fd14dd84
Emmanuel Gonzalez <emmangonza98@gmail.com>
2018-08-08 00:46:03 -0500

    completed program 4

--- program4/exg5388_p4.s
+++ program4/exg5388_p4.s
@@ -7,18 +7,32 @@
 main:
    BL _scanf				@checks for first input, n
    CMP R0, #0
-   BEQ _exit
+   BEQ _exit				@exits program if n = 0
+   MOV R1, R0
 
-   PUSH {R0}				@backup n for _divide call
+   PUSH {R1}				@backup n which will be altered in _scanf
 
    BL _scanf				@checks for second input, d
    CMP R0, #0
-   BEQ _exit
-   MOV R1, R0
+   BEQ _exit				@exits program if d = 0
+   MOV R2, R0
 
-   POP {R0}
+   POP {R1}				@restore n
 
-   BL _divide
+   VMOV S0, R1			@move the numerator to floating point register
+   VMOV S1, R2			@move the denominator to floating point register
+
+   BL _print_int
+
+   VCVT.F32.S32 S0, S0	@convert signed bit representation to single float
+   VCVT.F32.S32 S1, S1	@convert signed bit representation to single float
+
+   VDIV.F32 S2, S0, S1		@compute S2 = S0 / S1
+
+   VCVT.F64.F32 D4, S2	@covert the result to double precision for printing
+   VMOV R1, R2, D4		@split the double VFP register into two ARM registers
+
+   BL _print_float
 
    B main
 
@@ -26,43 +40,23 @@ _exit:
    MOV R7, #1
    SWI 0
 
-_scanf:
-    PUSH {LR}                @ store LR since scanf call overwrites
-    SUB SP, SP, #4          @ make room on stack
-    LDR R0, =format_str     @ R0 contains address of format string
-    MOV R1, SP              @ move SP to R1 to store entry on stack
-    BL scanf                @ call scanf
-    LDR R0, [SP]            @ load value at SP into R0
-    ADD SP, SP, #4          @ restore the stack pointer
-    POP {PC}                 @ return
-
-_divide:
+_scanf:					@procedure for scanning integers from scanf.s
    PUSH {LR}
-
-   VMOV S0, R0
-   VMOV S1, R1
-
-   @BL _print_int
-
-   VCVT.F32.U32 S0, S0
-   VCVT.F32.U32 S1, S1
-
-   VDIV.F32 S2, S0, S1
-
-   VCVT.F64.F32 D4, S2
-   VMOV R1, R2, D4
-
-   BL _print_float
-
+   SUB SP, SP, #4
+   LDR R0, =format_str
+   MOV R1, SP
+   BL scanf
+   LDR R0, [SP]
+   ADD SP, SP, #4
    POP {PC}
 
-_print_int:
+_print_int:				@prints the integers in the format "n / d = "
    PUSH {LR}
    LDR R0, =result_str1
    BL printf
    POP {PC}
 
-_print_float:
+_print_float:				@prints the single floating point value using R1 and R2
    PUSH {LR}
    LDR R0, =result_str2
    BL printf
